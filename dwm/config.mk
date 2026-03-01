6b5ce40d8481c0904e060814986589c49071dae8
daloserchamp <jktchamp10@gmail.com>
2026-03-01 15:43:25 -0600

    vscode gdb debugging

--- dwm/config.mk
+++ dwm/config.mk
@@ -28,7 +28,9 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+#CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS} #not debugging i guess
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -g -O0 ${INCS} ${CPPFLAGS} #debugging
+
 LDFLAGS  = ${LIBS}
 
 # Solaris
